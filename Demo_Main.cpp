6d3b91d0a3e7023793391ca23e49fd8924e014ae
El-Gendy-00 <62804966+El-Gendy-00@users.noreply.github.com>
2020-03-28 20:15:45 +0200

    Second

--- Demo_Main.cpp
+++ Demo_Main.cpp
@@ -1,14 +1,14 @@
 //#include "Drawing.h"
 #include "Rest\Restaurant.h"
 #include "GUI\GUI.h"
-
+//this is a test
 int main()
 {
-	
+
 	Restaurant* pRest = new Restaurant;
 	pRest->RunSimulation();
-	
+
 	delete pRest;
-	
+
 	return 0;
 }
