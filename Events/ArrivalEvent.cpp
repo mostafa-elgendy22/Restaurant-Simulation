173b5d714078482086f1f0b3f6309d6f37190608
El-Gendy-00 <62804966+El-Gendy-00@users.noreply.github.com>
2020-04-05 17:47:32 +0200

    Add files via upload

--- Events/ArrivalEvent.cpp
+++ Events/ArrivalEvent.cpp
@@ -2,9 +2,11 @@
 #include "..\Rest\Restaurant.h"
 
 
-ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType):Event(eTime, oID)
+ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType, double oMoney, int size) :Event(eTime, oID)
 {
 	OrdType = oType;
+	OrdMoney = oMoney;
+	OrdSize = size;
 }
 
 void ArrivalEvent::Execute(Restaurant* pRest)
@@ -12,10 +14,10 @@ void ArrivalEvent::Execute(Restaurant* pRest)
 	//This function should create an order and fills its data 
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
 
-	
-	
+
+
 	///For the sake of demo, this function will just create an order and add it to DemoQueue
 	///Remove the next code lines in phases 1&2
-	Order* pOrd = new Order(OrderID,OrdType);
+	Order* pOrd = new Order(OrderID, OrdType, OrdSize, EventTime, OrdMoney);
 	pRest->AddtoDemoQueue(pOrd);
 }
--- Events/ArrivalEvent.h
+++ Events/ArrivalEvent.h
@@ -8,11 +8,11 @@
 class ArrivalEvent: public Event
 {
 	//info about the order ralted to arrival event
-	int OrdDistance;	//order distance
+	int OrdSize;	//order distance
 	ORD_TYPE OrdType;		//order type: Normal, vegan, VIP	                
 	double OrdMoney;	//Total order money
 public:
-	ArrivalEvent(int eTime, int oID, ORD_TYPE oType);
+	ArrivalEvent(int eTime, int oID, ORD_TYPE oType,double oMoney, int size);
 	//Add more constructors if needed
 	
 	virtual void Execute(Restaurant *pRest);	//override execute function
