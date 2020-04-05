bf6b025b991d156c992714a19349a45997a856a2
El-Gendy-00 <62804966+El-Gendy-00@users.noreply.github.com>
2020-04-05 17:46:50 +0200

    Add files via upload

--- Rest/Order.cpp
+++ Rest/Order.cpp
@@ -1,10 +1,13 @@
 #include "Order.h"
 
-Order::Order(int id, ORD_TYPE r_Type)
+Order::Order(int id, ORD_TYPE r_Type,int size, int time, double money)
 {
 	ID = (id>0&&id<1000)?id:0;	//1<ID<999
 	type = r_Type;
 	status = WAIT;
+	Size = size;
+	totalMoney = money;
+	ArrTime = time;
 }
 
 Order::~Order()
--- Rest/Order.h
+++ Rest/Order.h
@@ -10,7 +10,7 @@ protected:
 	int ID;         //Each order has a unique ID (from 1 --> 999 )
 	ORD_TYPE type;		//order type: Normal, vegan, VIP
 	ORD_STATUS status;	//waiting, in-service, done
-	int Distance;	//The distance (in meters) between the order location and the resturant 
+	int Size;	//The distance (in meters) between the order location and the resturant 
 	                
 	double totalMoney;	//Total order money
 
@@ -22,7 +22,7 @@ protected:
 	//
 
 public:
-	Order(int ID, ORD_TYPE r_Type);
+	Order(int ID, ORD_TYPE r_Type,int size,int time,double money);
 	virtual ~Order();
 
 	int GetID();
