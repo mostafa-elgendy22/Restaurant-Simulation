8782138758568237e5b59b2c1672336fc75819cf
batyousefx <57029588+batyousefx@users.noreply.github.com>
2020-04-13 12:17:58 +0200

    Update Order.cpp

--- Code/Restaurant/Rest/Order.cpp
+++ Code/Restaurant/Rest/Order.cpp
@@ -1,38 +1,83 @@
 #include "Order.h"
 
-Order::Order(int id, ORD_TYPE r_Type,int size, int time, double money)
+
+
+Order::Order(int id, ORD_TYPE r_Type, int size, int time, double money)
+
 {
-	ID = (id>0&&id<1000)?id:0;	//1<ID<999
+	ID = (id > 0 && id < 1000) ? id : 0;	//1<ID<999
+
 	type = r_Type;
+
 	status = WAIT;
+
 	Size = size;
+
 	totalMoney = money;
+
 	ArrTime = time;
 }
 
+
+
 Order::~Order()
 {
+
 }
 
 int Order::GetID()
+
 {
 	return ID;
 }
 
 
+
+
+
 ORD_TYPE Order::GetType() const
+
 {
 	return type;
 }
 
 
+
+
+
+void Order::Setsize(int d)
+
+{
+	Size = d > 0 ? d : 0;
+}
+
+
+
+int Order::Getsize() const
+
+{
+	return Size;
+}
+
+
+
+
+
 void Order::setStatus(ORD_STATUS s)
+
 {
 	status = s;
 }
 
+
+
 ORD_STATUS Order::getStatus() const
+
 {
 	return status;
 }
 
+bool Order::operator> (const Order& x)const
+{
+	return (totalMoney > x.totalMoney);
+}
