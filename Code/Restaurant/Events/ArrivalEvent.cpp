da5aea020e2cb0b7d04f8ba065650406ff5582e2
batyousefx <57029588+batyousefx@users.noreply.github.com>
2020-04-13 12:11:46 +0200

    Update ArrivalEvent.cpp

--- Code/Restaurant/Events/ArrivalEvent.cpp
+++ Code/Restaurant/Events/ArrivalEvent.cpp
@@ -1,8 +1,9 @@
 #include "ArrivalEvent.h"
+
 #include "..\Rest\Restaurant.h"
 
-
 ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType, double oMoney, int size) :Event(eTime, oID)
+
 {
 	OrdType = oType;
 	OrdMoney = oMoney;
@@ -10,14 +11,27 @@ ArrivalEvent::ArrivalEvent(int eTime, int oID, ORD_TYPE oType, double oMoney, in
 }
 
 void ArrivalEvent::Execute(Restaurant* pRest)
+
 {
 	//This function should create an order and fills its data 
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
 
-
-
 	///For the sake of demo, this function will just create an order and add it to DemoQueue
 	///Remove the next code lines in phases 1&2
 	Order* pOrd = new Order(OrderID, OrdType, OrdSize, EventTime, OrdMoney);
-	pRest->AddtoDemoQueue(pOrd);
+
+	switch (OrdType)
+	{
+
+	case TYPE_NRM:
+		pRest->addtonormarlist(pOrd);
+		break;
+	case TYPE_VIP:
+		pRest->addtoviplist(pOrd);
+		break;
+	case TYPE_VGAN:
+		pRest->addtoveganlist(pOrd);
+		break;
+	}
+
 }
